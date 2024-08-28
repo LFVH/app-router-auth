8b09cedcf98772e08136e708265a95b21530dd77
LFVH <luizfelipevh@gmail.com>
2024-08-27 22:18:47 -0300

    Update page.tsx

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -18,8 +18,9 @@ export default function Page() {
           className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
           href="https://wa.me/5541999070610"
         >
-		whatsapp
+		Mange msg pro nosso zapzap
         </Link>
+	<a href="https://wa.me/5541999070610"><img src="whatsapp.svg" alt="Whatsapp" style="width:131px;height:31px;"></a>
       </div>
     </div>
   );
