4a2462e95f5dcd29b35c4359aa4d938616ae5351
LFVH <luizfelipevh@gmail.com>
2024-08-30 23:44:30 -0300

    Revert "Update upload.ts"
    This reverts commit f265d89c63f4f683869587ec683eba022fa76ae6.

--- api/upload.mjs
+++ api/upload.mjs
@@ -20,7 +20,7 @@ export const config = {
 };
 
 const parseForm = (req: NextApiRequest): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
-  const form = new formidable.IncomingForm({ uploadDir: uploadDir, keepExtensions: true});
+  const form = formidable.IncomingForm({ uploadDir: uploadDir, keepExtensions: true});
   fs.ensureDirSync(uploadDir);
   
 
