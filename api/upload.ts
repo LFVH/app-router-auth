b40c012ff47dfbc369fb8ab66d49afe2c2fb6a46
LFVH <luizfelipevh@gmail.com>
2024-09-01 16:59:22 -0300

    alteracoes

--- api/upload.ts
+++ api/upload.ts
@@ -1,6 +1,5 @@
-
+import { put } from '@vercel/blob';
 import type { NextApiResponse, NextApiRequest, PageConfig } from 'next';
-const { put } = require('@vercel/blob');
  
 export default async function handler(
   request: NextApiRequest,
