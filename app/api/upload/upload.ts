015e222daf3f645a9c17015f144e5934a7b2a24e
LFVH <luizfelipevh@gmail.com>
2024-09-01 22:38:09 -0300

    Update upload.ts

--- app/api/upload/upload.ts
+++ app/api/upload/upload.ts
@@ -1,11 +1,12 @@
 import { put } from '@vercel/blob';
 import { NextResponse } from 'next/server';
+import { PassThrough } from stream;
  
 export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const filename = searchParams.get('filename');
  
-  const blob = await put((filename !== null ? filename : ""), request.body, {
+  const blob = await put((filename !== null ? filename : ""), request.body !== null ? request.body : new PassThrough().end(), {
     access: 'public',
   });
  
