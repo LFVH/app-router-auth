b3a15ff6ecc0d5d3ee828c478d90750bea59bd1e
LFVH <luizfelipevh@gmail.com>
2024-09-01 22:00:51 -0300

    Update page.tsx

--- app/(public)/insertImages/page.tsx
+++ app/(public)/insertImages/page.tsx
@@ -21,7 +21,7 @@ export default function AvatarUploadPage() {
           const file = inputFileRef.current.files[0];
  
           const response = await fetch(
-            '/apps/api/upload?filename=${file.name}',
+            '/app/api/upload?filename=${file.name}',
             {
               method: 'POST',
               body: file,
