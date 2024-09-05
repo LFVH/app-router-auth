f97483fc29f528de35e17272d272676f4822ead8
LFVH <luizfelipevh@gmail.com>
2024-09-04 22:28:41 -0300

    tentando fazer o layout funcionar
    ./app/admin/layout.tsx
    Error:
      x Unexpected token `div`. Expected jsx identifier

--- app/admin/insertimages/layout.tsx
+++ app/admin/insertimages/layout.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link';
 import { MenuIcon } from '@/components/ui/icons';
+import LogoutButton from './logout-button';
 
 const links = [
   { href: 'https://signupandloginlfvh.vercel.app', title: 'Home' },
-  { href: '/admin/insertImages', title: 'Insert Images' },
+  { href: '/admin/insertimages', title: 'Insert Images' },
+  { href: '/admin/dashboard', title: 'Dashboard' },
   { href: '/listImages', title: 'List Images' },
   { href: '/contact', title: 'Contact' },
+  
 ];
 
 export default function Layout({
@@ -25,10 +28,9 @@ export default function Layout({
             ))}
           </nav>
           <div className="hidden items-center space-x-4 md:flex">
-            <Link
-              className="rounded-md border px-4 py-1.5 text-sm font-medium transition-colors hover:border-black hover:bg-black hover:text-white"
-              href="/login"
-            >
+		  <div className="border-t p-4">
+            <LogoutButton />
+          </div>
               Login
             </Link>
           </div>
