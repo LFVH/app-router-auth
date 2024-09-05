16db97f4c7fe617850ed1f857c58ea0ed00aaf70
LFVH <luizfelipevh@gmail.com>
2024-09-04 23:29:53 -0300

    Update route.ts

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -1,16 +1,12 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 import { db } from '@/drizzle/db';
 import { images } from '@/drizzle/schema';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ message: 'Method not allowed' });
-  }
-
+export async function GET(request: Request): Promise<NextResponse> {
   try {
     const result = await db.select().from(images).execute();
-    res.status(200).json(result);
+    return NextResponse.status(200).json(result);
   } catch (error) {
-    res.status(500).json({ message: 'Failed to fetch images', error });
+    NextResponse.status(500).json({ message: 'Failed to fetch images', error });
   }
 }
