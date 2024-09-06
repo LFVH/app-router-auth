2a623a28869121436fd1b3c36ab419f80884ed8e
LFVH <luizfelipevh@gmail.com>
2024-09-05 22:49:27 -0300

    Update page.tsx

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,15 +1,15 @@
 import Link from 'next/link';
 
 export default function Page() {
+console.log('teste');
   return (
     <div className="container grid items-center gap-4 px-4 text-center md:px-6 lg:gap-10">
       <div className="space-y-3">
         <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
-          Welcome to our Platform
+          Welcome
         </h1>
         <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-          The all-in-one platform for building, deploying, and managing modern
-          web apps.
+          weeeeelcomeee
         </p>
       </div>
       <div className="flex flex-col justify-center gap-2 min-[400px]:flex-row">
